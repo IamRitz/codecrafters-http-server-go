3b29eb8a2b95539beebdabd75b61647d9d070b59
IamRitz <26003726+IamRitz@users.noreply.github.com>
2024-04-01 05:21:15 +0530

    pass 7th stage

--- app/server.go
+++ app/server.go
@@ -8,28 +8,12 @@ import (
     "strings"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+func handleConnection(conn net.Conn) {
 
-	// Uncomment this block to pass the first stage
-
-	l, err := net.ListenAndServe("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-
-    var conn net.Conn
-
-    conn, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+    defer conn.Close()
 
     buffer := make([]byte, 1024)
-    _, err = conn.Read(buffer)
+    _, err := conn.Read(buffer)
 
     if err != nil{
 		fmt.Println("Error reading data from connection: ", err.Error())
@@ -81,5 +65,31 @@ func main() {
     if err != nil{
 		fmt.Println("Error writing response: ", err.Error())
 		os.Exit(1)
+    }
+}
+
+func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Println("Logs from your program will appear here!")
+
+	// Uncomment this block to pass the first stage
+
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
+    defer l.Close()
+    var conn net.Conn
+
+    for {
+
+        conn, err = l.Accept()
+        if err != nil {
+            fmt.Println("Error accepting connection: ", err.Error())
+            os.Exit(1)
+        }
+
+        go handleConnection(conn)
     }
 }
